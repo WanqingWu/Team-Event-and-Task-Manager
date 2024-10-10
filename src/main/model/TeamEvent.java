8e72e2e9f4e28e1619629d235393829618c1ed35
wwu32 <wwu32@student.ubc.ca>
2024-10-10 16:58:23 -0700

    Added specification for TeamEvent class

---
+++ src/main/model/TeamEvent.java
@@ -0,0 +1,42 @@
+package model;
+import java.util.List;
+import java.util.ArrayList;
+
+// Represents a team event.
+public class TeamEvent {
+    
+    public String name;
+    public int time;
+    public List<Member> memberList;
+
+    // EFFECTS: construct a teamevent with a event name, time, and an empty member list.
+    public TeamEvent() {
+        // stub
+    }
+
+    public void setName(String name) {
+        // stub
+    }
+
+    public void setTime(int time) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add member to this event
+    public void addMember() {
+        // stub
+    }
+
+    public String getName() {
+        return "";
+    } 
+    
+    public int getTime() {
+        return 0;
+    }
+
+    public List<Member> getMemberList() {
+        return null;
+    }
+}
