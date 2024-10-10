7daa00e30d620045fe0ee5b820a48e75269fd558
wwu32 <wwu32@student.ubc.ca>
2024-10-10 00:05:21 -0700

    Added specification of Member class

---
+++ src/main/model/Member.java
@@ -0,0 +1,40 @@
+package model;
+
+// Represents a team member having a name, birthday and task.
+public class Member {
+    private String name;
+    private int bday;
+    private Task task;
+
+    /*
+     * EFFECTS: constructs a team member, name of the member is set to name; 
+     *          birthday of the member is set to bday; 
+     *          task of the member is set to null.
+     */
+    public Member(String name, int bday) {
+        this.name = name;
+        this.bday = bday;
+        this.task = null;
+    }
+
+    public String getName() {
+        return ""; //stub
+    }
+
+    public int getBday() {
+        return 0; //stub
+    }
+
+    public Task getTask() {
+        return task; //stub
+    }
+
+    /*
+     * REQUIRES: this member doesn't have a current task
+     * MODIFIES: this
+     * EFFECTS: assign a task to this member
+     */
+    public void addTask() {
+        // TODO
+    }
+}
