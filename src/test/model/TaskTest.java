115ec5f420a9f6b6a94932a96536bb6caf985749
wwu32 <wwu32@student.ubc.ca>
2024-10-10 02:15:39 -0700

    Added tests for Task class

---
+++ src/test/model/TaskTest.java
@@ -0,0 +1,61 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    private Member member;
+    private Task testTask;
+
+    @BeforeEach
+    void runBefore() {
+        testTask = new Task();
+        member = new Member("June", 20030609);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("not started", testTask.getStatus());
+        assertEquals(null, testTask.getMember());
+    }
+
+    @Test
+    void testGetStatus() {
+        assertEquals("not started", testTask.getStatus());
+        testTask.workOnTask();
+        assertEquals("in progress", testTask.getStatus());
+        testTask.completeTask();
+        assertEquals("completed", testTask.getStatus());
+    }
+
+    @Test
+    void testGetMember() {
+        assertEquals(null, testTask.getMember());
+        testTask.assignTaskTo(member);
+        assertEquals(member, testTask.getMember());
+    }
+
+    @Test
+    void testAssignTaskTo() {
+        testTask.assignTaskTo(member);
+        assertEquals(member, testTask.getMember());
+    }
+
+    @Test
+    void testWorkOnTask() {
+        assertEquals("not started", testTask.getStatus());
+        testTask.workOnTask();
+        assertEquals("in progress", testTask.getStatus());
+    }
+
+    @Test
+    void testCompleteTask() {
+        assertEquals("not started", testTask.getStatus());
+        testTask.completeTask();
+        assertEquals("completed", testTask.getStatus());
+    }
+}
