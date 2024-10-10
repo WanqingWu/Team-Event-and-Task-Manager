df7f0cd85a6052de0c106a34c4aa08c752f11545
wwu32 <wwu32@student.ubc.ca>
2024-10-10 00:18:48 -0700

    Added tests for Member class

---
+++ src/test/model/MemberTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberTest {
+
+    private Member testMember;
+    private Task task;
+
+    @BeforeEach
+    void runBefore() {
+        testMember = new Member("June", 20030609);
+        task = new Task();
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("June", testMember.getName());
+        assertEquals(20030609, testMember.getBday());
+        assertEquals(null, testMember.getTask());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("June", testMember.getName());
+    }
+
+    @Test
+    void testGetBday() {
+        assertEquals(20030609, testMember.getBday());
+    }
+
+    @Test
+    void testGetTask(){
+        assertEquals(null, testMember.getTask());
+        testMember.addTask(task);
+        assertEquals(task, testMember.getTask());
+    }
+
+    @Test
+    void testAddTask() {
+        testMember.addTask(task);
+        assertEquals(task, testMember.getTask());
+    }
+}
