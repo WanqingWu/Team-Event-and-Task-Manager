8f51d12c8f74905f1554c79f025762d6360536ab
wwu32 <wwu32@student.ubc.ca>
2024-10-11 09:20:31 -0700

    Added implementation and updated tests for TimeSlot class

--- src/main/model/TimeSlot.java
+++ src/main/model/TimeSlot.java
@@ -1,6 +1,7 @@
 package model;
 // Represents a time slot.
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class TimeSlot {
@@ -9,48 +10,72 @@ public class TimeSlot {
     public ArrayList<Member> bookedMembers;
     public int startTime;
     public int endTime;
+    public ArrayList<TeamEvent> events;
     public boolean isAvailable;
 
     // EFFECTS: constructs a timeslot with date set to date, 
     //          start time set to startTime, end time set to endTime,
     //          bookedMembers set to an empty list, and is available.
     public TimeSlot(String date, int startTime, int endTime) {
-        // stub
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.bookedMembers = new ArrayList<>();
+        this.events = new ArrayList<>();
+        this.isAvailable = true;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds team event to time slot
+    public void addEvent(TeamEvent event) {
+        this.events.add(event);
     }
 
     // MODIFIES: this
     // EFFECTS: returns true if this timeslot is available 
     //          (there is no team event during this timeslot)
     public boolean isAvailable() {
-        return true; //stub
+        for (TeamEvent teamEvent: this.events) {
+            if (this.startTime >= teamEvent.getStartTime() && this.startTime < teamEvent.getEndTime()) {
+                return false;
+            }
+            else if (this.endTime > teamEvent.getStartTime() && this.endTime <= teamEvent.getEndTime()){
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getDate() {
-        return ""; // stub
+        return this.date;
     }
 
     public int getStartTime() {
-        return 0; // stub
+        return this.startTime;
     }
 
     public int getEndTime() {
-        return 0; // stub
+        return this.endTime;
     }
 
     public List<Member> getBookedMembers() {
-        return null; // stub
+        return this.bookedMembers;
+    }
+
+    public List<TeamEvent> getEvents() {
+        return this.events;
     }
 
     // REQUIRES: this time slot is available
     // MODIFIES: this
     // EFFECTS: books this time slot
     public void bookSlot(Member member) {
-        // stub
+        this.bookedMembers.add(member);
     }
 
     // MODIFIES: this
     // EFFECTS: returns the number of booked members
     public int getNumberOfBookedMembers() {
-        return 0; //stub
+        return this.bookedMembers.size();
     }
 }
--- src/test/model/TimeSlotTest.java
+++ src/test/model/TimeSlotTest.java
@@ -18,6 +18,10 @@ public class TimeSlotTest {
     private TeamEvent teamEvent1;
     private TeamEvent teamEvent2;
 
+    private List<TeamEvent> events0;
+    private List<TeamEvent> events1;
+    private List<TeamEvent> events2;
+
     private List<Member> bookedMembers0;
     private List<Member> bookedMembers1;
     private List<Member> bookedMembers2;
@@ -34,8 +38,23 @@ public class TimeSlotTest {
         testTimeSlot3 = new TimeSlot("Oct 28", 13, 16);
 
         teamEvent1 = new TeamEvent("party");
+        teamEvent1.setStartTime(10);
+        teamEvent1.setEndTime(13);
         teamEvent2 = new TeamEvent("meeting");
+        teamEvent2.setStartTime(15);
+        teamEvent2.setEndTime(16);
 
+        events0 = new ArrayList<>();
+        events1 = new ArrayList<>();
+        events1.add(teamEvent1);
+        events2 = new ArrayList<>();
+        events2.add(teamEvent1);
+        events2.add(teamEvent2);
+
+        member1 = new Member("June", 20030609);
+        member2 = new Member("Stephen", 20010927);
+        member3 = new Member("Mina", 20040506);
+        
         bookedMembers0 = new ArrayList<>();
         bookedMembers1 = new ArrayList<>();
         bookedMembers1.add(member1);
@@ -46,10 +65,6 @@ public class TimeSlotTest {
         bookedMembers3.add(member1);
         bookedMembers3.add(member2);
         bookedMembers3.add(member3);
-
-        member1 = new Member("June", 20030609);
-        member2 = new Member("Stephen", 20010927);
-        member3 = new Member("Mina", 20040506);
     }
 
     @Test
@@ -59,17 +74,29 @@ public class TimeSlotTest {
         assertEquals(11, testTimeSlot1.getEndTime());
         assertEquals(bookedMembers0, testTimeSlot1.getBookedMembers());
         assertEquals(true, testTimeSlot1.isAvailable());
+        assertEquals(events0, testTimeSlot1.getEvents());
+    }
+
+    @Test
+    void testAddEvent() {
+        assertEquals(events0, testTimeSlot1.getEvents());
+        testTimeSlot1.addEvent(teamEvent1);
+        assertEquals(events1, testTimeSlot1.getEvents());
+        testTimeSlot1.addEvent(teamEvent2);
+        assertEquals(events2, testTimeSlot1.getEvents());
     }
 
     @Test
     void testIsAvailable() {
-        teamEvent1.setStartTime(10);
-        teamEvent1.setEndTime(13);
+        testTimeSlot1.addEvent(teamEvent1);
         assertEquals(false, testTimeSlot1.isAvailable());
+        testTimeSlot2.addEvent(teamEvent1);
         assertEquals(true, testTimeSlot2.isAvailable());
+        testTimeSlot3.addEvent(teamEvent1);
         assertEquals(true, testTimeSlot3.isAvailable());
-        teamEvent2.setStartTime(15);
-        teamEvent2.setEndTime(16);
+        
+        testTimeSlot2.addEvent(teamEvent2);
+        testTimeSlot3.addEvent(teamEvent2);
         assertEquals(false, testTimeSlot2.isAvailable());
         assertEquals(false, testTimeSlot3.isAvailable());
     }
@@ -88,13 +115,13 @@ public class TimeSlotTest {
 
     @Test
     void testGetNumberOfBookedMembers() {
-        assertEquals(0, testTimeSlot1.getBookedMembers());
+        assertEquals(0, testTimeSlot1.getNumberOfBookedMembers());
         testTimeSlot1.bookSlot(member1);
-        assertEquals(1, testTimeSlot1.getBookedMembers());
+        assertEquals(1, testTimeSlot1.getNumberOfBookedMembers());
         testTimeSlot1.bookSlot(member2);
-        assertEquals(2, testTimeSlot1.getBookedMembers());
+        assertEquals(2, testTimeSlot1.getNumberOfBookedMembers());
         testTimeSlot1.bookSlot(member3);
-        assertEquals(3, testTimeSlot1.getBookedMembers());
+        assertEquals(3, testTimeSlot1.getNumberOfBookedMembers());
     }
         
 }
