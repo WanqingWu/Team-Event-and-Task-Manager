74fc723b4258de926580d3cbc43226216f63ef0d
wwu32 <wwu32@student.ubc.ca>
2024-10-25 06:55:00 -0700

    Added implementation for JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,9 +1,10 @@
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
 package persistence;
 
 import model.TeamEvent;
-
-// Referenced from the JsonSerialization Demo
-// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+import model.Member;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -19,36 +20,52 @@ public class JsonReader {
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-        // stub
+        this.source = source;
     }
 
     // EFFECTS: reads team event from file and returns it;
     // throws IOException if an error occurs reading data from file
     public TeamEvent readTeamEvent() throws IOException {
-        TeamEvent tm = new TeamEvent("");
-        return tm;// stub
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseTeamEvent(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return ""; // stub
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses team event from JSON object and returns it
     private TeamEvent parseTeamEvent(JSONObject jsonObject) {
-        TeamEvent tm = new TeamEvent("");
-        return tm; // stub
+        String name = jsonObject.getString("name");
+        TeamEvent te = new TeamEvent(name);
+        addMembersToTeamEvent(te, jsonObject);
+        return te;
     }
 
-    // MODIFIES: wr
+    // MODIFIES: te
     // EFFECTS: parses members from JSON object and adds them to team event
     private void addMembersToTeamEvent(TeamEvent te, JSONObject jsonObject) {
-        // stub
+        JSONArray jsonArray = jsonObject.getJSONArray("members");
+        for (Object json : jsonArray) {
+            JSONObject nextMember = (JSONObject) json;
+            addMemberToTeamEvent(te, nextMember);
+        }
     }
 
-    // MODIFIES: wr
+    // MODIFIES: te
     // EFFECTS: parses member from JSON object and adds it to team event
     private void addMemberToTeamEvent(TeamEvent te, JSONObject jsonObject) {
-        // stub
+        String name = jsonObject.getString("name");
+        String bday = jsonObject.getString("birthday");
+        Member member = new Member(name, Integer.valueOf(bday));
+        te.addMember(member);
     }
 }
