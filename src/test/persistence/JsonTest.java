17aaf15b145461987239eb29be4921065778b13b
wwu32 <wwu32@student.ubc.ca>
2024-10-24 17:41:43 -0700

    Added JsonTest class

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,16 @@
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+package persistence;
+
+import model.Member;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    protected void checkMember(String name, int bday, Task task, Member member) {
+        assertEquals(name, member.getName());
+        assertEquals(bday, member.getBday());
+        assertEquals(task, member.getTask());
+    }
+}
