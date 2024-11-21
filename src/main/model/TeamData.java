4ac9c7b3fd6b3499123233c7993328eb15181afa
wwu32 <wwu32@student.ubc.ca>
2024-11-20 18:32:33 -0800

    Specified a TeamData class

---
+++ src/main/model/TeamData.java
@@ -0,0 +1,25 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Represents team data containing team events and team projects.
+public class TeamData {
+    private List<TeamEvent> teamEvents;
+    private List<TeamProject> teamProjects;
+
+    // Constructs a team data containing team events and team projects
+    public TeamData(List<TeamEvent> teamEvents, List<TeamProject> teamProjects) {
+        // this.teamEvents = teamEvents;
+        // this.teamProjects = teamProjects;
+    }
+
+    public List<TeamEvent> getTeamEvents() {
+        // return teamEvents;
+    }
+
+    public List<TeamProject> getTeamProjects() {
+        // return teamProjects;
+    }
+}
