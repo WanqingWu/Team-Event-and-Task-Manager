f28ebe90eebc3c253751f4d35b33f0fc9591f8cc
wwu32 <wwu32@student.ubc.ca>
2024-11-29 04:33:48 -0800

    Passed the checkstyle

--- src/main/ui/WindowCloseListener.java
+++ src/main/ui/WindowCloseListener.java
@@ -16,7 +16,9 @@ public class WindowCloseListener implements WindowListener {
     }
 
     @Override
-    public void windowOpened(WindowEvent e) {}
+    public void windowOpened(WindowEvent e) {
+
+    }
 
     @Override
     public void windowClosing(WindowEvent e) {
@@ -24,19 +26,29 @@ public class WindowCloseListener implements WindowListener {
     }
 
     @Override
-    public void windowClosed(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {
+
+    }
 
     @Override
-    public void windowIconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {
+
+    }
 
     @Override
-    public void windowDeiconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {
+
+    }
 
     @Override
-    public void windowActivated(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {
+
+    }
 
     @Override
-    public void windowDeactivated(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {
+        
+    }
 
     private void printLoggedEvent() {
         System.out.println("Logged Events:");
